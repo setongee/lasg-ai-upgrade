4f07ca704b896055c7479b487cd0e36058cc28b0
setongee <setongee@gmail.com>
2026-08-17 15:24:17 +0100

    env

--- src/api/read/environment.js
+++ src/api/read/environment.js
@@ -2,13 +2,13 @@ const environment = {
   test: 'http://localhost:8083/api/v2',
   live: 'https://web3.lagosstate.gov.ng/api/v2',
 };
-const env = environment.test;
+const env = environment.live;
 
 const frontend_url_env = {
   test: 'http://localhost:5173',
   live: 'https://lagosstate.gov.ng',
 };
 
-const frontend_url = frontend_url_env.test;
+const frontend_url = frontend_url_env.live;
 
 export { env, frontend_url };
